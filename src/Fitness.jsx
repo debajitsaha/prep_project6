93d269f7f12432d9a9cd250a90a51b8d4cf519bf
debajitsaha <81859187+debajitsaha89@users.noreply.github.com>
2023-08-09 18:21:33 +0530

    done

--- src/Fitness.jsx
+++ src/Fitness.jsx
@@ -1,14 +1,13 @@
-import React, { useContext } from 'react'
-import DataStore from './DataStore';
+
 
 function Fitness() {
-    const Context=useContext(DataStore);
+   
     
     
   return (
     <div>
       <h1>Fitness</h1>
-      <p>{Context.data[1].heading}</p>
+      
     </div>
   )
 }
