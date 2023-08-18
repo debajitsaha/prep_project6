24b4137ab5c553bda9dfd8177cb936b75d6a020a
debajitsaha <81859187+debajitsaha89@users.noreply.github.com>
2023-08-18 19:41:08 +0530

    done

--- src/RouteCompo.js
+++ src/RouteCompo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import{ Route, BrowserRouter, Routes}from 'react-router-dom';
+import{ Link,Route, BrowserRouter, Routes}from 'react-router-dom';
 import Home from './Home';
 import Bollywood from './Bollywood';
 import Techonology from './Techonology';
